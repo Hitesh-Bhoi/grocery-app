062c509c0b1941a176d66f6fde904fb554d875e5
bhoi hitesh <bhoihitesh1492@gmail.com>
2025-11-24 04:11:58 +0530

    added footer ui

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,150 @@
-'use client';
-import { StyledH1 } from "./footer.styled";
-require('./footer.styled');
+"use client";
+import Image from "next/image";
+import logo from "../../../public/logo.svg";
+import location from "../../../public/icons/location.svg";
+import headphone from "../../../public/icons/headphone.svg";
+import email from "../../../public/icons/email.svg";
+import clock from "../../../public/icons/clock.svg";
+import appStore from "../../../public/images/apple-appstore.png"
+import playStore from "../../../public/images/google-playstore.png"
+import mastercard from "../../../public/icons/mastercard.svg"
+import UPI from "../../../public/icons/upi.svg"
+import {
+  StyledAccountInfo,
+  StyledAddressSection,
+  StyledCompanyInfo,
+  StyledContactSection,
+  StyledCorporateInfo,
+  StyledEmailSection,
+  StyledFollowusInfo,
+  StyledFooterContainer,
+  StyledInstallAppInfo,
+  StyledLogoSection,
+  StyledLogoWithInfo,
+  StyledPaymentsLink,
+  StyledPhoneInfo,
+  StyledPopularProductInfo,
+  StyledRightsInfo,
+  StyledStoreLinks,
+  StyleFooterContactDetails,
+  StyleFooterInfoDetails,
+} from "./footer.styled";
 
-const Footer=()=>{
-    return(
-        <>
-        <StyledH1>footer</StyledH1>
-        </>
-    )
-}
+require("./footer.styled");
+
+const Footer = () => {
+  return (
+    <>
+      <StyledFooterContainer>
+        <StyleFooterInfoDetails>
+          <div className="first-tab-size-div">
+          <StyledLogoWithInfo>
+            <StyledLogoSection>
+              <Image src={logo} alt="website logo" />
+            </StyledLogoSection>
+            <div className="footer-child-div">
+            <StyledAddressSection>
+              <Image src={location} alt="address icon" width={20} height={20} />
+              <span className="footer-text">
+                Address 5171 W Champbell Ave Kent, Utah 53127 United States
+              </span>
+            </StyledAddressSection>
+            <StyledContactSection>
+              <Image
+                src={headphone}
+                alt="headphone icon"
+                width={20}
+                height={20}
+              />
+              <span>
+                Call Us{" "}
+                <span className="contact-number">(+91)-540-025-124553</span>
+              </span>
+            </StyledContactSection>
+            </div>
+            <div className="footer-child-div">
+            <StyledEmailSection>
+              <Image src={email} alt="email icon" width={20} height={20} />
+              <p className="email-box">
+                <span>Email</span> <span className="email">sales@ecobazar.com</span>
+              </p>
+            </StyledEmailSection>
+            <StyledEmailSection>
+              <Image src={clock} alt="clock icon" width={20} height={20} />
+              <span>Hours 10:00 - 18:00, Mon - Sat</span>
+            </StyledEmailSection>
+            </div>
+          </StyledLogoWithInfo>
+          <div className="combine-footer-div">
+          <StyledCompanyInfo>
+            <p className="title">Company</p>
+            <span>About Us</span>
+            <span>Privacy Policy</span>
+            <span>Terms & Conditions</span>
+            <span>Contact Us</span>
+            <span>Support Center</span>
+            <span>Careers</span>
+          </StyledCompanyInfo>
+          <StyledAccountInfo>
+            <p className="title">Account</p>
+            <span>Sign In</span>
+            <span>View Chart</span>
+            <span>My Wishlist</span>
+            <span>Tract My Order</span>
+            <span>Help Ticket</span>
+            <span>Shipping Details</span>
+            <span>Compare Products</span>
+          </StyledAccountInfo>
+          </div>
+          </div>
+          <div className="second-tab-size-div">
+          <div className="combine-footer-div">
+          <StyledCorporateInfo>
+            <p className="title">Corporate</p>
+            <span>Become a Vendor</span>
+            <span>Affiliate Program</span>
+            <span>Farm Business</span>
+            <span>Farm Careers</span>
+            <span>Our Suppliers</span>
+            <span>Accessibilitty</span>
+            <span>Promotion</span>
+          </StyledCorporateInfo>
+          <StyledPopularProductInfo>
+            <p className="title">Popular</p>
+            <span>Milk & Flavoured Milk</span>
+            <span>Butter and Margarine</span>
+            <span>Eggs Substitutes</span>
+            <span>Marmalades</span>
+            <span>Sour Cream and Dips</span>
+            <span>Tea & Kombucha</span>
+            <span>Cheese</span>
+          </StyledPopularProductInfo>
+          </div>
+          <StyledInstallAppInfo>
+            <p className="title">
+                Install App
+            </p>
+            <span>From App Store or Google Play</span>
+            <StyledStoreLinks>
+                <Image src={appStore} alt="app store link" width={100} height={45}/>
+                <Image src={playStore} alt="play store link" width={100} height={45}/>
+            </StyledStoreLinks>
+            <span>Secure Payment Gatways</span>
+            <StyledPaymentsLink>
+                <Image src={mastercard} alt="mastercard link" width={100} height={50}/>
+                <Image src={UPI} alt="UPI link" width={100} height={45}/>
+            </StyledPaymentsLink>
+          </StyledInstallAppInfo>
+          </div>
+        </StyleFooterInfoDetails>
+
+        <StyleFooterContactDetails>
+          <StyledRightsInfo></StyledRightsInfo>
+          <StyledPhoneInfo></StyledPhoneInfo>
+          <StyledFollowusInfo></StyledFollowusInfo>
+        </StyleFooterContactDetails>
+      </StyledFooterContainer>
+    </>
+  );
+};
 export default Footer;
--- src/components/footer/footer.styled.tsx
+++ src/components/footer/footer.styled.tsx
@@ -1,7 +1,174 @@
-'use client';
+"use client";
 import styled from "styled-components";
 
-export const StyledH1 = styled.h1`
-    font-size: 34px;
-    color: blue;
-`
+export const StyledFooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  box-shadow: rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em;
+`;
+export const StyleFooterInfoDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: 0px;
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media ((min-width: 768px) and (max-width: 1023px)) {
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+`;
+export const StyledLogoWithInfo = styled.div`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 15px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+
+  @media ((min-width: 768px) and (max-width: 1023px)) {
+    width: 70%;
+  }
+`;
+export const StyledCompanyInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 15px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+`;
+export const StyledAccountInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 15px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+`;
+export const StyledCorporateInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 15px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+`;
+export const StyledPopularProductInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 15px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 20px;
+  }
+`;
+export const StyledInstallAppInfo = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 10px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media ((min-width: 768px) and (max-width: 1023px)) {
+    width: 70%;
+  }
+`;
+export const StyledLogoSection = styled.div``;
+export const StyledAddressSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+
+  @media (max-width: 767px) {
+    width: 50%;
+    align-items: flex-start;
+  }
+`;
+export const StyledContactSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  .contact-number {
+    color: #20b820;
+  }
+
+  @media (max-width: 767px) {
+    width: 50%;
+    align-items: flex-start;
+  }
+`;
+export const StyledEmailSection = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  .email {
+    color: #20b820;
+    word-wrap: wrap;
+  }
+
+  @media (max-width: 767px) {
+    width: 50%;
+    align-items: flex-start;
+  }
+`;
+export const StyledStoreLinks = styled.div``;
+export const StyledPaymentsLink = styled.div``;
+
+export const StyleFooterContactDetails = styled.div``;
+export const StyledRightsInfo = styled.div``;
+export const StyledPhoneInfo = styled.div``;
+export const StyledFollowusInfo = styled.div``;
